17b9e27f24dd2c3232150a0c55e6cb00ea9ebb7d
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-12-17 14:49:02 -0500

    some fixes

--- comandos/pem.js
+++ comandos/pem.js
@@ -3,17 +3,18 @@ const SerpApi = require('google-search-results-nodejs')
 const search = new SerpApi.GoogleSearch(process.env.serpapiKey)
 const fs = require('fs');
 
+
 module.exports.run = async (client, message, args) => {
     const q = args.join(" ");
+
+    const text = fs.readFileSync(__dirname + '/../txts/pem.txt', 'utf8');
 
     if (!q) return client.sendText(
         message.from,
         `Você não me deu uma pesquisa para fazer. Exemplo: !pem restaurantes em ariquemes`
     );
 
-    const data = fs.readFileSync(__dirname + '/../txts/pem.txt', 'utf8');
-
-    if (!data) return client.sendText(
+    if (!text) return client.sendText(
         message.from,
         `Nao localizei meu template (pem.txt) na pasta "txts".`
     );
@@ -28,11 +29,11 @@ module.exports.run = async (client, message, args) => {
         const data = result.local_results;
 
         data.forEach(i => {
-            if (i.phone === undefined &&
-                !i.phone.replaceAll("+", "").replaceAll(" ", "").replaceAll("-", "").slice(4).startsWith("9"))
-                return;
-
-            numbers.push(`${i.phone.replaceAll("+", "").replaceAll(" ", "").replaceAll("-", "")}@c.us`);
+            if (i.phone === undefined)return;
+            const formated = i.phone.replaceAll("+", "").replaceAll(" ", "").replaceAll("-", "")
+            if (formated.slice(4).startsWith("9")) {
+                numbers.push(`${formated}@c.us`);
+            }
         });
 
         if (numbers.length === 0) return client.sendText(
@@ -40,14 +41,35 @@ module.exports.run = async (client, message, args) => {
             `Ocorreu um problema com a pesquisa.`
         );
 
-        numbers.forEach(num => {
-            setTimeout(() => {
-                client.sendText(
-                    num,
-                    data.toString() + `\n\n${(Math.random() + 1).toString(36).substring(7)}`
-                );
+       /* numbers.forEach(num => {
+            setTimeout(async () => {
+                try {
+                    client.sendText(
+                        num,
+                        text.toString()
+                    );
+                    await client
+                        .sendFile(
+                            num,
+                            __dirname + '/../medias/proposta.pdf',
+                            'Proposta',
+                            `Aqui está a proposta!`
+                        )
+                    await client
+                        .sendFile(
+                            num,
+                            __dirname + '/../medias/video.mp4',
+                            'Vídeo',
+                            ``
+                        )
+                } catch (error) {
+                    console.log(error)
+                }
+                
             }, 3000)
-        })
+        })*/
+
+        console.log(numbers)
 
         return client.sendText(
             message.from,
